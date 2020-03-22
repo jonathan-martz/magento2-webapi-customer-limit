bb4255a401c37392d7f2b8d27141594751ab0cd9
Jonathan Martz <git@jmartz.de>
2020-03-22 22:39:20 +0100

    fix customer limit, add config stuff

--- Plugin/Rest/Api.php
+++ Plugin/Rest/Api.php
@@ -3,11 +3,14 @@
 namespace JonathanMartz\WebApiCustomerLimit\Plugin\Rest;
 
 use JonathanMartz\WebApiLog\Model\RequestFactory;
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress;
 use Magento\Framework\UrlInterface;
 use Magento\Webapi\Controller\Rest;
 use Psr\Log\LoggerInterface;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+use \Magento\Framework\Controller\Result\JsonFactory;
 use function json_encode;
 
 /**
@@ -34,6 +37,19 @@ class Api
      * @var RemoteAddress
      */
     private $remote;
+    /**
+     * @var Session
+     */
+    private $customerSession;
+
+    /**
+    * @var \Magento\Framework\Controller\Result\JsonFactory
+    */
+    protected $resultJsonFactory;
+
+    protected $scopeConfig;
+
+    const CONFIG_LIMIT = 'webapi-customer-limit/general/daily-limit';
 
     /**
      * Api constructor.
@@ -46,12 +62,16 @@ class Api
         LoggerInterface $logger,
         UrlInterface $url,
         RemoteAddress $remote,
-        RequestFactory $webapilog
+        RequestFactory $webapilog,
+        JsonFactory $resultJsonFactory,
+        ScopeConfigInterface $scopeConfig
     ) {
         $this->logger = $logger;
         $this->url = $url;
         $this->remote = $remote;
         $this->webapistats = $webapilog;
+        $this->resultJsonFactory = $resultJsonFactory;
+        $this->scopeConfig = $scopeConfig;
     }
 
     /**
@@ -65,16 +85,24 @@ class Api
         callable $proceed,
         RequestInterface $request
     ) {
-        $url = str_replace([$this->url->getBaseUrl(), 'rest/V1/'], ['', ''], $this->url->getCurrentUrl());
+        $url = str_replace([$this->url->getBaseUrl(), 'rest/V1/', 'index.php'], ['', '',''], $this->url->getCurrentUrl());
+        $url = trim($url, '/');
+        
         $ip = $this->remote->getRemoteAddress();
 
-        $requestFactory = $this->requestFactory->create();
+        $model = $this->webapistats->create();
+        $collection = $model->getCollection();
+        $collection->addFieldToFilter('ip' , ['eq' => sha1($ip)]);
 
-        $collection = $requestFactory->getCollection();
-        $collection->addFieldToFilter('ip', array('eq' => sha1($ip)));
+        var_dump($this->scopeConfig->getValue(self::CONFIG_LIMIT, 'stores'));
 
-        var_dump(count($collection));
-        
+        if(count($collection) > 100){
+            $this->logger->alert('User blocked: ' . sha1($ip));
+            $resultJson = $this->resultJsonFactory->create();
+            $resultJson->setData(['message' => 'Already created 100 users today.']);
+            return $resultJson;
+        }
+
         return $proceed($request);
     }
 }
